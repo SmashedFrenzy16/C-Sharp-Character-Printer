75172b508b8ee590f441771fb07fb043dcf0b561
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2023-11-20 10:46:46 +0000

    Add program

---
+++ CharacterPrinter/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+
+int x;
+
+x = 0;
+
+while (x < 256)
+{
+    Console.WriteLine($"{x}: {(char)x}");
+
+    x++;
+}
